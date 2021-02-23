f3d9a8fae790e98a354014bcfbf65c82b7e63e1c
Anand Devarajan <ananddevarajan01@gmail.com>
2021-02-23 09:35:36 +0530

    changed color

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -18,7 +18,7 @@ function Navbar() {
           <Link to='#' className='menu-bars'>
               <MenuIcon  style={{marginBottom: "25px"}} onClick={showSidebar}/>
           </Link>
-          <h3 className='text-muted ml-auto mr-auto'>Smart Grace Mark Calculator</h3>
+          <h3 className='text-white ml-auto mr-auto'>Smart Grace Mark Calculator</h3>
         </div>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
    
