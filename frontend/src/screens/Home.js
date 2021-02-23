416550ec79f32156bb473d6b736a5c228edcd5d7
Anand Devarajan <ananddevarajan01@gmail.com>
2021-02-23 12:57:39 +0530

    button bug fixed

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -12,55 +12,80 @@ const Home = () => {
           src='https://static.thenounproject.com/png/35785-200.png'
           thumbnail
         />
-        <Button variant='success' className='mt-3 text-white'>
-          <Link
-            to='/student/login'
-            style={{ textDecoration: 'none' }}
-            className='text-white'
+        <Link
+          to='/student/login'
+          style={{ textDecoration: 'none' }}
+          className='text-white'
+        >
+          <Button
+            variant='success'
+            className='mt-3 text-white'
+            style={{ width: '206px' }}
           >
             {' '}
             Sign in as student
-          </Link>
-        </Button>
-        <Button variant='primary' className='mt-3'>
-          <Link    to='/student/signup'
-            style={{ textDecoration: 'none' }}
-            className='text-white'> create student account</Link>
-        </Button>
+          </Button>
+        </Link>
+        <Link
+          to='/student/signup'
+          style={{ textDecoration: 'none' }}
+          className='text-white'
+        >
+          <Button variant='primary' className='mt-3' >
+            create student account
+          </Button>
+        </Link>
       </div>
       <div className='home__faculty'>
         <Image
           src='https://static.thenounproject.com/png/2011000-200.png'
           thumbnail
         />
-        <Button variant='success' className='mt-3'>
-          <Link    to='/faculty/login'
-            style={{ textDecoration: 'none' }}
-            className='text-white'>Sign in as Faculty</Link>
-        </Button>
-        <Button variant='primary' className='mt-3'>
-          <Link    to='/faculty/signup'
-            style={{ textDecoration: 'none' }}
-            className='text-white'>create Faculty account</Link> 
-        </Button>
+        <Link
+          to='/faculty/login'
+          style={{ textDecoration: 'none' }}
+          className='text-white'
+        >
+          <Button variant='success' className='mt-3'  style={{ width: '203px' }}>
+            Sign in as Faculty
+          </Button>
+        </Link>
+        <Link
+          to='/faculty/signup'
+          style={{ textDecoration: 'none' }}
+          className='text-white'
+        >
+          <Button variant='primary' className='mt-3'>
+            create Faculty account
+          </Button>
+        </Link>
       </div>
-      
+
       <div className='home__admin'>
         <Image
           src='https://static.thenounproject.com/png/371299-200.png'
           thumbnail
         />
-        <Button variant='success' className='mt-3'>
-          <Link    to='/admin/login'
-            style={{ textDecoration: 'none' }}
-            className='text-white'> Sign in as Admin</Link>
-        </Button>
-        <Button variant='primary' className='mt-3'>
-          {' '}
-          <Link    to='/admin/signup'
-            style={{ textDecoration: 'none' }}
-            className='text-white'> create Admin account</Link>
-        </Button>
+        <Link
+          to='/admin/login'
+          style={{ textDecoration: 'none' }}
+          className='text-white'
+        >
+          <Button variant='success' className='mt-3'  style={{ width: '194px' }}>
+            {' '}
+            Sign in as Admin
+          </Button>
+        </Link>
+        <Link
+          to='/admin/signup'
+          style={{ textDecoration: 'none' }}
+          className='text-white'
+        >
+          <Button variant='primary' className='mt-3'>
+            {' '}
+            create Admin account
+          </Button>
+        </Link>
       </div>
     </div>
   );
