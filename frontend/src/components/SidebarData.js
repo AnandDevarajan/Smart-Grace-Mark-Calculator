b9383518c2887e25867ddc356a4953d8f90fa670
Anand Devarajan <ananddevarajan01@gmail.com>
2021-02-23 09:06:57 +0530

    Added login icon

--- frontend/src/components/SidebarData.js
+++ frontend/src/components/SidebarData.js
@@ -3,8 +3,15 @@ import HomeIcon from '@material-ui/icons/Home';
 import PeopleIcon from '@material-ui/icons/People';
 import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
 import MenuBookIcon from '@material-ui/icons/MenuBook';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
 
 export const SidebarData = [
+  {
+    title: 'Login',
+    path: '/login',
+    icon: <PersonAddIcon/>,
+    cName: 'nav-text',
+  },
   {
     title: 'Profile',
     path: '/',
