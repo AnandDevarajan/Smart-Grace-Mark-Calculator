7751fba76fc002d544f235f0088334c38de2d377
Anand Devarajan <45593286+AnandDevarajan@users.noreply.github.com>
2021-04-13 15:56:59 +0530

    range

--- frontend/src/screens/Admin/SetGrade.js
+++ frontend/src/screens/Admin/SetGrade.js
@@ -6,6 +6,7 @@ import {
   gradeRangeDetails,
   updateGradeRange,
 } from '../../actions/courseActions';
+import { GRADE_RANGE_UPDATE_RESET } from '../../constants/courseConstants';
 import { Table, Button } from 'react-bootstrap';
 import axios from 'axios';
 
@@ -23,35 +24,42 @@ const SetGrade = ({ match, history }) => {
   const [C, setC] = useState('');
   const [P, setP] = useState('');
   const [F, setF] = useState('');
+  const [generate, setGenerate] = useState(false);
   const [grs, setGrs] = useState([]);
 
   const dispatch = useDispatch();
 
   const adminSignin = useSelector((state) => state.adminSignin);
   const { adminInfo } = adminSignin;
-
-  const gradeRange = useSelector((state) => state.gradeRange);
-  const { grade } = gradeRange;
+  const gradeRangeUpdate = useSelector((state) => state.gradeRangeUpdate);
+  const { error, success } = gradeRangeUpdate;
 
   useEffect(() => {
-    if (!adminInfo) {
-      history.push('/');
-    }
-    dispatch(gradeRangeDetails(cid));
-    axios
-      .get(`/course/get/report/${cid}`)
-      .then((response2) => {
-        console.log('hi', response2);
-        setMaxMark(response2.data.report[0].Max);
-        setMinMark(response2.data.report[0].Min);
-      })
-      .catch((error) => {
-        console.log(error);
+    if (success) {
+      dispatch({
+        type: GRADE_RANGE_UPDATE_RESET,
       });
-  }, [adminInfo]);
-
-  console.log(grade);
-  const setGradeRange = (max, min) => {
+    } else if (adminInfo) {
+      axios
+        .all([
+          axios.get(`/course/graderange/${cid}`),
+          axios.get(`/course/get/report/${cid}`),
+        ])
+        .then(
+          axios.spread((response1, response2) => {
+            console.log('hi', response1, response2);
+            setGrs(response1.data.grade);
+            setMaxMark(response2.data.report[0].Max);
+            setMinMark(response2.data.report[0].Min);
+          })
+        )
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [adminInfo, minMark, maxMark, grs, success]);
+  console.log(minMark, maxMark);
+  const setGradeRange = (max) => {
     if (max >= 80) {
       setO(`${max}-${max - 5}`);
       setAp(`${max - 6}-${max - 10}`);
@@ -89,68 +97,125 @@ const SetGrade = ({ match, history }) => {
       setP(`${max - 30}-${max - 39}`);
       setF(`${max - 40}-0`);
     }
-    axios
-      .put(`/course/graderange/${cid}`, { O, Ap, A, Bp, B, C, P, F })
-      .then((response) => {
-        console.log(response);
-        setGrs(response.data.grade);
-      });
+    setGenerate(!generate);
+    update(cid, O, Ap, A, Bp, B, C, P, F);
   };
-
+  const update = (cid, O, Ap, A, Bp, B, C, P, F) => {
+    dispatch(updateGradeRange(cid, O, Ap, A, Bp, B, C, P, F));
+  };
   return (
-    <div>
-      <Link to='/admin/course/report'>
-        <Button variant='light'>
-          <ArrowBackIcon /> Go Back
-        </Button>
-      </Link>
-      <h3 style={{ textTransform: 'capitalize' }}>
-        Grade Range - <span>{cname}</span>
-      </h3>
-      <p className='text-center'></p>
-      <Table striped bordered hover responsive className='table-sm'>
-        <thead>
-          <tr>
-            <th>Course ID</th>
+    <>
+      {generate === false && (
+        <div>
+          <Link to='/admin/course/report'>
+            <Button variant='light'>
+              <ArrowBackIcon /> Go Back
+            </Button>
+          </Link>
+          <h3 style={{ textTransform: 'capitalize' }}>
+            Grade Range - <span>{cname}</span>
+          </h3>
+          <p className='text-center'></p>
+          <Table striped bordered hover responsive className='table-sm'>
+            <thead>
+              <tr>
+                <th>Course ID</th>
 
-            <th>O</th>
-            <th>A+</th>
-            <th>A</th>
-            <th>B+</th>
-            <th>B</th>
-            <th>C</th>
-            <th>P</th>
-            <th>F</th>
-            <th>Set Grade</th>
-          </tr>
-        </thead>
-        <tbody>
-          {grade.map((gr) => (
-            <tr>
-              <td>{cid}</td>
-              <td>{gr.O}</td>
-              <td>{gr.Ap}</td>
-              <td>{gr.A}</td>
-              <td>{gr.Bp}</td>
-              <td>{gr.B}</td>
-              <td>{gr.C}</td>
-              <td>{gr.P}</td>
-              <td>{gr.F}</td>
-              <td>
-                <button
-                  className='btn btn-sm btn-success'
-                  onClick={() => {
-                    setGradeRange(maxMark, minMark);
-                  }}
-                >
-                  Generate
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </div>
+                <th>O</th>
+                <th>A+</th>
+                <th>A</th>
+                <th>B+</th>
+                <th>B</th>
+                <th>C</th>
+                <th>P</th>
+                <th>F</th>
+                <th>Set Grade</th>
+              </tr>
+            </thead>
+            <tbody>
+              {grs.map((gr) => (
+                <tr>
+                  <td>{cid}</td>
+                  <td>{gr.O}</td>
+                  <td>{gr.Ap}</td>
+                  <td>{gr.A}</td>
+                  <td>{gr.Bp}</td>
+                  <td>{gr.B}</td>
+                  <td>{gr.C}</td>
+                  <td>{gr.P}</td>
+                  <td>{gr.F}</td>
+                  <td>
+                    <button
+                      className='btn btn-sm btn-warning'
+                      onClick={() => {
+                        setGradeRange(maxMark);
+                      }}
+                    >
+                      Set
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+      )}
+      {generate === true && (
+        <div>
+          <Link to='/admin/course/report'>
+            <Button variant='light'>
+              <ArrowBackIcon /> Go Back
+            </Button>
+          </Link>
+          <h3 style={{ textTransform: 'capitalize' }}>
+            Grade Range - <span>{cname}</span>
+          </h3>
+          <p className='text-center'></p>
+          <Table striped bordered hover responsive className='table-sm'>
+            <thead>
+              <tr>
+                <th>Course ID</th>
+
+                <th>O</th>
+                <th>A+</th>
+                <th>A</th>
+                <th>B+</th>
+                <th>B</th>
+                <th>C</th>
+                <th>P</th>
+                <th>F</th>
+                <th>Set Grade</th>
+              </tr>
+            </thead>
+            <tbody>
+              {grs.map((gr) => (
+                <tr>
+                  <td>{cid}</td>
+                  <td>{gr.O}</td>
+                  <td>{gr.Ap}</td>
+                  <td>{gr.A}</td>
+                  <td>{gr.Bp}</td>
+                  <td>{gr.B}</td>
+                  <td>{gr.C}</td>
+                  <td>{gr.P}</td>
+                  <td>{gr.F}</td>
+                  <td>
+                    <button
+                      className='btn btn-sm btn-success'
+                      onClick={() => {
+                        setGradeRange(maxMark);
+                      }}
+                    >
+                      Generate
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+      )}
+    </>
   );
 };
 
