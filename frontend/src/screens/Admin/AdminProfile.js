37b83936611d4c4e866cbb64abce20ce4e8f5eb5
Anand Devarajan <45593286+AnandDevarajan@users.noreply.github.com>
2021-04-14 12:19:39 +0530

    publish results bug fixed

--- frontend/src/screens/Admin/AdminProfile.js
+++ frontend/src/screens/Admin/AdminProfile.js
@@ -1,26 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
-import EmailIcon from "@material-ui/icons/Email";
-import PersonIcon from "@material-ui/icons/Person";
-import PhoneIcon from "@material-ui/icons/Phone";
-import HomeIcon from "@material-ui/icons/Home";
-import PublishIcon from "@material-ui/icons/Publish";
-import EditTwoToneIcon from "@material-ui/icons/EditTwoTone";
-import CloseIcon from "@material-ui/icons/Close";
-import Message from "../../components/Message";
-import { Row, Col, ListGroup, Image, Container } from "react-bootstrap";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import EmailIcon from '@material-ui/icons/Email';
+import PersonIcon from '@material-ui/icons/Person';
+import PhoneIcon from '@material-ui/icons/Phone';
+import HomeIcon from '@material-ui/icons/Home';
+import PublishIcon from '@material-ui/icons/Publish';
+import EditTwoToneIcon from '@material-ui/icons/EditTwoTone';
+import CloseIcon from '@material-ui/icons/Close';
+import Message from '../../components/Message';
+import { Row, Col, ListGroup, Image, Container } from 'react-bootstrap';
+import axios from 'axios';
 
 const AdminProfile = ({ history }) => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [address, setAddress] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
   const [message, setMessage] = useState(null);
-  const [status, setStatus] = useState("");
+  const [status, setStatus] = useState('');
   const [value, onChange] = useState(new Date());
 
   const gracemarkCreate = useSelector((state) => state.gracemarkCreate);
@@ -31,7 +31,7 @@ const AdminProfile = ({ history }) => {
 
   useEffect(() => {
     if (!adminInfo) {
-      history.push("/");
+      history.push('/');
     }
     axios
       .all([
@@ -49,16 +49,16 @@ const AdminProfile = ({ history }) => {
           setStatus(response2.data.status);
         })
       );
-  }, [adminInfo, name, email, address, phone, status]);
+  }, [adminInfo, name, email, address, phone]);
 
   const publishResult = () => {
     const config = {
       headers: { Authorization: `Bearer ${adminInfo.token}` },
     };
     axios
-      .get("/admin/publish/result", config)
+      .get('/admin/publish/result', config)
       .then((response) => {
-        setStatus("Published");
+        setStatus(response.data.message);
       })
       .catch((error) => {
         console.log(error);
@@ -70,7 +70,7 @@ const AdminProfile = ({ history }) => {
       headers: { Authorization: `Bearer ${adminInfo.token}` },
     };
     axios
-      .get("/admin/reset/publish", config)
+      .get('/admin/reset/publish', config)
       .then((response) => {
         setStatus(response.data.status);
       })
@@ -79,58 +79,57 @@ const AdminProfile = ({ history }) => {
       });
   };
 
-  const msg = status;
-
-  console.log(message);
+  console.log('stat', status);
   return (
-    <Container fluid="sm" className="themed-container">
-      <button className="btn btn-primary">Welcome</button>
-      <Container className="mt-5 border border-left-0 border-right-0 border-dark">
+    <Container fluid='sm' className='themed-container'>
+      <button className='btn btn-primary mb-3'>Welcome</button>
+
+      <Container className='mt-5 border border-left-0 border-right-0 border-dark'>
         <hr></hr>
-        <Row className="mt-5">
+        <Row className='mt-5'>
           <Col md={2} sm={12}>
             <Container
-              className="border border-danger mt-5 mr-5"
-              style={{ width: "217px" }}
+              className='border border-danger mt-5 mr-5'
+              style={{ width: '217px' }}
             >
               <Image
-                className="mt-5 "
-                src="https://static.thenounproject.com/png/371299-200.png"
+                className='mt-5 '
+                src='https://static.thenounproject.com/png/371299-200.png'
                 style={{
-                  height: "200px",
-                  width: "200px",
-                  objectFit: "contain",
+                  height: '200px',
+                  width: '200px',
+                  objectFit: 'contain',
                 }}
               />
             </Container>
           </Col>
           <Col md={7} sm={12}>
-            <ListGroup className="mt-2 mb-4 ml-5">
+            <ListGroup className='mt-2 mb-4 ml-5'>
               <h4
-                className="text-center btn btn-block btn-warning"
-                style={{ backgroundColor: "#ce1212" }}
+                className='text-center btn btn-block btn-warning'
+                style={{ backgroundColor: '#ce1212' }}
               >
                 Admin Profile
               </h4>
               <ListGroup.Item
-                style={{ backgroundColor: "#1e212d", color: "#eeeeee" }}
+                style={{ backgroundColor: '#1e212d', color: '#eeeeee' }}
               >
-                <h4 style={{ textTransform: "capitalize" }}>
+                <h4 style={{ textTransform: 'capitalize' }}>
                   <PersonIcon />
                   :&nbsp;&nbsp;&nbsp;
                   {name}
                 </h4>
               </ListGroup.Item>
               <ListGroup.Item
-                style={{ backgroundColor: "#1e212d", color: "#eeeeee" }}
+                style={{ backgroundColor: '#1e212d', color: '#eeeeee' }}
               >
-                <h4 style={{ textTransform: "lowercase" }}>
+                <h4 style={{ textTransform: 'lowercase' }}>
                   <EmailIcon />
                   :&nbsp;&nbsp;&nbsp;{email}
                 </h4>
               </ListGroup.Item>
               <ListGroup.Item
-                style={{ backgroundColor: "#1e212d", color: "#eeeeee" }}
+                style={{ backgroundColor: '#1e212d', color: '#eeeeee' }}
               >
                 <h4>
                   <PhoneIcon />
@@ -139,9 +138,9 @@ const AdminProfile = ({ history }) => {
                 </h4>
               </ListGroup.Item>
               <ListGroup.Item
-                style={{ backgroundColor: "#1e212d", color: "#eeeeee" }}
+                style={{ backgroundColor: '#1e212d', color: '#eeeeee' }}
               >
-                <h4 style={{ textTransform: "capitalize" }}>
+                <h4 style={{ textTransform: 'capitalize' }}>
                   <HomeIcon />
                   :&nbsp;&nbsp;&nbsp;{address}
                 </h4>
@@ -149,53 +148,72 @@ const AdminProfile = ({ history }) => {
             </ListGroup>
           </Col>
           <Col md={3} sm={12}>
-            <ListGroup className=" ml-2" style={{ marginTop: "7px" }}>
-              {msg == "Not Published" && (
-                <Message variant="warning">Results not published</Message>
+            <ListGroup className=' ml-2' style={{ marginTop: '7px' }}>
+              {status ===
+                'Unable to Publish Results now. Mark allocation is yet to be completed' && (
+                <Message variant='danger'>{status}</Message>
               )}
-              {msg == "Published" && (
-                <Message variant="success">Results published</Message>
+              {status === 'Published' && (
+                <Message variant='success'>Results published</Message>
+              )}
+              {status === 'Not Published' && (
+                <Message variant='warning'>
+                  Results not published
+                </Message>
               )}
               <ListGroup.Item>
-                <h6 className=" text-center text-white btn btn-sm btn-block btn-primary">
+                <h6 className=' text-center text-white btn btn-sm btn-block btn-primary'>
                   Actions
                 </h6>
               </ListGroup.Item>
               <ListGroup.Item>
                 <Link to={`/admin/edit/profile/${adminInfo.result.adminID}`}>
-                  <h6 style={{ textTransform: "Capitalize" }}>
+                  <h6 style={{ textTransform: 'Capitalize' }}>
                     <EditTwoToneIcon />
                     &nbsp;&nbsp;Edit Profile
                   </h6>
                 </Link>
               </ListGroup.Item>
               <ListGroup.Item>
-                <h6 style={{ textTransform: "capitalize" }}>
-                  {status === "Not Published" && (
+                <h6 style={{ textTransform: 'capitalize' }}>
+                  {status ===
+                    'Unable to Publish Results now. Mark allocation is yet to be completed' && (
                     <>
-                      <PublishIcon className="mr-2" />
+                      <PublishIcon className='mr-2' />
                       <button
-                        className="btn btn-sm btn-info"
+                        className='btn btn-sm btn-info'
                         onClick={publishResult}
-                        style={{ width: "120px" }}
+                        style={{ width: '120px' }}
                       >
                         Publish Results
                       </button>
                     </>
                   )}
-                  {status === "Published" && (
+                  {status === 'Not Published' && (
                     <>
-                      <PublishIcon className="mr-2" />
+                      <PublishIcon className='mr-2' />
                       <button
-                        className="btn btn-sm btn-warning mr-5"
+                        className='btn btn-sm btn-info'
                         onClick={publishResult}
-                        style={{ width: "86px" }}
+                        style={{ width: '120px' }}
+                      >
+                        Publish Results
+                      </button>
+                    </>
+                  )}
+                  {status === 'Published' && (
+                    <>
+                      <PublishIcon className='mr-2' />
+                      <button
+                        className='btn btn-sm btn-warning mr-5'
+                        onClick={publishResult}
+                        style={{ width: '86px' }}
                       >
                         Published
                       </button>
                       <CloseIcon
-                        className="icon"
-                        style={{ color: "red" }}
+                        className='icon'
+                        style={{ color: 'red' }}
                         onClick={resetPublish}
                       />
                     </>
