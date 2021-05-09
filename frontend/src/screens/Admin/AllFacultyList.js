46164385b98a194753dab4701687df544f7132aa
Anand Devarajan <45593286+AnandDevarajan@users.noreply.github.com>
2021-05-09 23:29:44 +0530

    updated faculty list

--- frontend/src/screens/Admin/AllFacultyList.js
+++ frontend/src/screens/Admin/AllFacultyList.js
@@ -40,86 +40,82 @@ const AllFacultyList = ({ history }) => {
   };
 
   return (
-    <div className="container-fluid d-flex justify-content-center">
-      <div
-        className="ml-5 align-items-center alllist_div"
-        style={{ backgroundColor: "white" }}
-      >
-        <Link to="/admin/profile" className="goback">
-          <Button variant="light">
-            <ArrowBackIcon /> Go Back
-          </Button>
-        </Link>
+    <div
+      className="ml-5 align-items-center alllist_div"
+      style={{ backgroundColor: "white" }}
+    >
+      <Link to="/admin/profile" className="goback">
+        <Button variant="light">
+          <ArrowBackIcon /> Go Back
+        </Button>
+      </Link>
 
-        <div className="card ml-5 px-3 overflow my_card">
-          {message != null && <Message variant="success">{message}</Message>}
-          <h1 className="text-center list_heading text-info">Faculty LIST</h1>
-          {error ? (
-            <Message variant="danger">{error}</Message>
-          ) : (
-            <Table striped bordered hover responsive className="table-sm">
-              <thead>
-                <tr>
-                  <th>Faculty ID</th>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Phone</th>
-                  <th>Department</th>
-                  <th>Course ID</th>
-                  <th>Class Adviser</th>
-                  <th>Batch</th>
-                  <th>Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                {faculties.map((faculty) => (
-                  <tr key={faculty.FacultyID}>
-                    <td>{faculty.FacultyID}</td>
-                    <td>{faculty.Name}</td>
-                    <td>
-                      <a href={`mailto:${faculty.EmailID}`}>
-                        {faculty.EmailID}
-                      </a>
-                    </td>
-                    <td>{faculty.PhoneNum}</td>
-                    <td>{faculty.Department}</td>
-                    <td>{faculty.CourseID}</td>
-                    <td>
-                      {faculty.ClassAdviser === "Yes" ? (
-                        <CheckIcon style={{ color: "green" }} />
-                      ) : (
-                        <CloseIcon style={{ color: "red" }} />
-                      )}
-                    </td>
-                    {faculty.Batch !== "N/A" ? (
-                      <td>{faculty.Batch}</td>
+      <div className="card  overflow my_card">
+        {message != null && <Message variant="success">{message}</Message>}
+        <h1 className="text-center list_heading text-info">Faculty LIST</h1>
+        {error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Table striped bordered hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>Faculty ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Department</th>
+                <th>Course ID</th>
+                <th>Class Adviser</th>
+                <th>Batch</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {faculties.map((faculty) => (
+                <tr key={faculty.FacultyID}>
+                  <td>{faculty.FacultyID}</td>
+                  <td>{faculty.Name}</td>
+                  <td>
+                    <a href={`mailto:${faculty.EmailID}`}>{faculty.EmailID}</a>
+                  </td>
+                  <td>{faculty.PhoneNum}</td>
+                  <td>{faculty.Department}</td>
+                  <td>{faculty.CourseID}</td>
+                  <td>
+                    {faculty.ClassAdviser === "Yes" ? (
+                      <CheckIcon style={{ color: "green" }} />
                     ) : (
-                      <td>-</td>
+                      <CloseIcon style={{ color: "red" }} />
                     )}
-                    {faculty.completion === "Yes" && (
-                      <td>
-                        {" "}
-                        <DoneAllIcon
-                          className="icon"
-                          style={{ color: "green" }}
-                        />
-                      </td>
-                    )}
-                    {faculty.completion !== "Yes" && (
-                      <td>
-                        <NotificationsIcon
-                          onClick={() => notifyFaculty(faculty.FacultyID)}
-                          className="icon"
-                          style={{ height: "20px", color: "#ff1414" }}
-                        />
-                      </td>
-                    )}
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          )}
-        </div>
+                  </td>
+                  {faculty.Batch !== "N/A" ? (
+                    <td>{faculty.Batch}</td>
+                  ) : (
+                    <td>-</td>
+                  )}
+                  {faculty.completion === "Yes" && (
+                    <td>
+                      {" "}
+                      <DoneAllIcon
+                        className="icon"
+                        style={{ color: "green" }}
+                      />
+                    </td>
+                  )}
+                  {faculty.completion !== "Yes" && (
+                    <td>
+                      <NotificationsIcon
+                        onClick={() => notifyFaculty(faculty.FacultyID)}
+                        className="icon"
+                        style={{ height: "20px", color: "#ff1414" }}
+                      />
+                    </td>
+                  )}
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
       </div>
     </div>
   );
