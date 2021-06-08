1181baea7ef731f446ebb313645fbe77573b41a0
Anand Devarajan <45593286+AnandDevarajan@users.noreply.github.com>
2021-06-08 22:43:49 +0530

    auth test

--- spec/LoginTest.spec.js
+++ spec/LoginTest.spec.js
@@ -1,66 +1,70 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const { expect } = chai;
 const BASE_URL = "http://127.0.0.1:5000";
 chai.use(chaiHttp);
 
-describe("Authentication Unit", () => {
-  describe("boundary condition", () => {
-    it("Invalid Email and Password", (done) => {
+let unit = describe("Authentication Unit", () => {
+  let condition1 = describe("boundary condition", () => {
+    let spec1 = it("Invalid Email and Password", (done) => {
       chai
         .request(BASE_URL)
         .post("/student/login")
         .send({ email: "", password: "" })
         .end((err, res) => {
           if (err) done(err);
-          expect(res).to.have.status(400);
-          expect(res).to.be.an("object");
-          expect(res.body.message).to.be.an("String");
+          expect(res.status).toBe(400);
+          expect(res.body.message).toEqual(jasmine.any(String));
+          console.log("1️⃣ ", unit.description, "\n");
+          console.log(condition1.description);
           done();
+          console.log("✔",spec1.result.description, "\n");
         });
     });
   });
-  describe("negative condition", () => {
-    it("Invalid Email", (done) => {
+  let condition2 = describe("negative condition", () => {
+    let spec2 = it("Invalid Email", (done) => {
       chai
         .request(BASE_URL)
         .post("/student/login")
         .send({ email: "student@test123.com", password: "test123" })
         .end((err, res) => {
           if (err) done(err);
-          expect(res).to.have.status(400);
-          expect(res).to.be.an("object");
-          expect(res.body.message).to.equal("Invalid Email");
+          expect(res.status).toBe(400);
+          expect(res.body.message).toEqual("Invalid Email");
+          console.log(condition2.description);
           done();
+          console.log("✔",spec2.result.description);
         });
     });
-    it("Invalid Password", (done) => {
+    let spec3 = it("Invalid Password", (done) => {
       chai
         .request(BASE_URL)
         .post("/student/login")
         .send({ email: "student@test.com", password: "test1234" })
         .end((err, res) => {
           if (err) done(err);
-          expect(res).to.have.status(400);
-          expect(res).to.be.an("object");
-          expect(res.body.message).to.equal("Invalid Password");
+          expect(res.status).toBe(400);
+          expect(res.body.message).toEqual("Invalid Password");
           done();
+          console.log("✔",spec3.result.description, "\n");
         });
     });
   });
-  describe("positive condition", () => {
-    it("Auth Success", (done) => {
+  let condition3 = describe("positive condition", () => {
+    let spec4 = it("Auth Success", (done) => {
       chai
         .request(BASE_URL)
         .post("/student/login")
         .send({ email: "student@test.com", password: "test123" })
         .end((err, res) => {
           if (err) done(err);
-          expect(res).to.have.status(200);
-          expect(res).to.be.an("object");
-          expect(res.body.result).to.be.an("object");
-          expect(res.body.token).to.be.an("string");
+          expect(res.status).toBe(200);
+          expect(res).toEqual(jasmine.any(Object));
+          expect(res.body.result).toEqual(jasmine.any(Object));
+          expect(res.body.token).toEqual(jasmine.any(String));
+          console.log(condition3.description);
           done();
+          console.log("✔",spec4.result.description, "\n");
         });
     });
   });
--- spec/cgpaCalculation.spec.js
+++ spec/cgpaCalculation.spec.js
@@ -1,117 +1,117 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const { expect } = chai;
-const BASE_URL = "http://127.0.0.1:5000";
-chai.use(chaiHttp);
+// const chai = require("chai");
+// const chaiHttp = require("chai-http");
+// const { expect } = chai;
+// const BASE_URL = "http://127.0.0.1:5000";
+// chai.use(chaiHttp);
 
-describe("CGPA Calculation", () => {
-  describe("boundary condition", () => {
-    it("{F,4},{F,4},{F,1},{F,3},{F,2} => CGPA = 4.00", (done) => {
-      chai
-        .request(BASE_URL)
-        .put("/student/caluclate/cgpa/anand123")
-        .send({
-          marks: [
-            { Final_Grade: "F", Grade: "F", credits: "4" },
-            { Final_Grade: "F", Grade: "F", credits: "4" },
-            { Final_Grade: "F", Grade: "F", credits: "1" },
-            { Final_Grade: "F", Grade: "F", credits: "3" },
-            { Final_Grade: "F", Grade: "F", credits: "2" },
-          ],
-        })
-        .end((err, res) => {
-          if (err) done(err);
-          expect(res).to.have.status(200);
-          expect(res).to.be.an("object");
-          expect(res.body.final_cgpa).to.equal("4.00");
-          done();
-        });
-    });
-    it("{O,4},{O,4},{O,1},{O,3},{O,2} => CGPA = 10.00", (done) => {
-      chai
-        .request(BASE_URL)
-        .put("/student/caluclate/cgpa/anand123")
-        .send({
-          marks: [
-            { Final_Grade: "O", Grade: "O", credits: "4" },
-            { Final_Grade: "O", Grade: "O", credits: "4" },
-            { Final_Grade: "O", Grade: "O", credits: "1" },
-            { Final_Grade: "O", Grade: "O", credits: "3" },
-            { Final_Grade: "O", Grade: "O", credits: "2" },
-          ],
-        })
-        .end((err, res) => {
-          if (err) done(err);
-          expect(res).to.have.status(200);
-          expect(res).to.be.an("object");
-          expect(res.body.final_cgpa).to.equal("10.00");
-          done();
-        });
-    });
-  });
-  describe("negative condition", () => {
-    it("{B+,0},{B,0},{A+,0},{A,0},{O,0} => CGPA = Invalid", (done) => {
-      chai
-        .request(BASE_URL)
-        .put("/student/caluclate/cgpa/anand123")
-        .send({
-          marks: [
-            { Final_Grade: "B++", Grade: "B", credits: "0" },
-            { Final_Grade: "B", Grade: "B", credits: "0" },
-            { Final_Grade: "A+", Grade: "A+", credits: "0" },
-            { Final_Grade: "A", Grade: "A", credits: "0" },
-            { Final_Grade: "O", Grade: "O", credits: "0" },
-          ],
-        })
-        .end((err, res) => {
-          if (err) done(err);
-          expect(res).to.have.status(400);
-          expect(res.body.message).to.equal("Unable to calculate cgpa now");
-          done();
-        });
-    });
-    it("{O,0},{O,0},{A+,0},{` `,0},{` `,0} => CGPA = Invalid", (done) => {
-      chai
-        .request(BASE_URL)
-        .put("/student/caluclate/cgpa/anand123")
-        .send({
-          marks: [
-            { Final_Grade: "O", Grade: "O", credits: "4" },
-            { Final_Grade: "B+", Grade: "B+", credits: "4" },
-            { Final_Grade: "A+", Grade: "A+", credits: "2" },
-            { Final_Grade: " ", Grade: " ", credits: "1" },
-            { Final_Grade: " ", Grade: " ", credits: "3" },
-          ],
-        })
-        .end((err, res) => {
-          if (err) done(err);
-          expect(res).to.have.status(400);
-          expect(res.body.message).to.equal("Unable to calculate cgpa now");
-          done();
-        });
-    });
-  });
-  describe("positive condition", () => {
-    it("{B+,4},{B,4},{A+,1},{A,3},{O,2} => CGPA = 8.32", (done) => {
-      chai
-        .request(BASE_URL)
-        .put("/student/caluclate/cgpa/anand123")
-        .send({
-          marks: [
-            { Final_Grade: "B+", Grade: "B", credits: "4" },
-            { Final_Grade: "B", Grade: "B", credits: "4" },
-            { Final_Grade: "A+", Grade: "A+", credits: "1" },
-            { Final_Grade: "A", Grade: "A", credits: "3" },
-            { Final_Grade: "O", Grade: "O", credits: "2" },
-          ],
-        })
-        .end((err, res) => {
-          if (err) done(err);
-          expect(res).to.have.status(200);
-          expect(res).to.be.an("object");
-          expect(res.body.final_cgpa).to.equal("8.32");
-          done();
-        });
-    });
-  });
-});
+// describe("CGPA Calculation", () => {
+//   describe("boundary condition", () => {
+//     it("{F,4},{F,4},{F,1},{F,3},{F,2} => CGPA = 4.00", (done) => {
+//       chai
+//         .request(BASE_URL)
+//         .put("/student/caluclate/cgpa/anand123")
+//         .send({
+//           marks: [
+//             { Final_Grade: "F", Grade: "F", credits: "4" },
+//             { Final_Grade: "F", Grade: "F", credits: "4" },
+//             { Final_Grade: "F", Grade: "F", credits: "1" },
+//             { Final_Grade: "F", Grade: "F", credits: "3" },
+//             { Final_Grade: "F", Grade: "F", credits: "2" },
+//           ],
+//         })
+//         .end((err, res) => {
+//           if (err) done(err);
+//           expect(res).to.have.status(200);
+//           expect(res).to.be.an("object");
+//           expect(res.body.final_cgpa).to.equal("4.00");
+//           done();
+//         });
+//     });
+//     it("{O,4},{O,4},{O,1},{O,3},{O,2} => CGPA = 10.00", (done) => {
+//       chai
+//         .request(BASE_URL)
+//         .put("/student/caluclate/cgpa/anand123")
+//         .send({
+//           marks: [
+//             { Final_Grade: "O", Grade: "O", credits: "4" },
+//             { Final_Grade: "O", Grade: "O", credits: "4" },
+//             { Final_Grade: "O", Grade: "O", credits: "1" },
+//             { Final_Grade: "O", Grade: "O", credits: "3" },
+//             { Final_Grade: "O", Grade: "O", credits: "2" },
+//           ],
+//         })
+//         .end((err, res) => {
+//           if (err) done(err);
+//           expect(res).to.have.status(200);
+//           expect(res).to.be.an("object");
+//           expect(res.body.final_cgpa).to.equal("10.00");
+//           done();
+//         });
+//     });
+//   });
+//   describe("negative condition", () => {
+//     it("{B+,0},{B,0},{A+,0},{A,0},{O,0} => CGPA = Invalid", (done) => {
+//       chai
+//         .request(BASE_URL)
+//         .put("/student/caluclate/cgpa/anand123")
+//         .send({
+//           marks: [
+//             { Final_Grade: "B++", Grade: "B", credits: "0" },
+//             { Final_Grade: "B", Grade: "B", credits: "0" },
+//             { Final_Grade: "A+", Grade: "A+", credits: "0" },
+//             { Final_Grade: "A", Grade: "A", credits: "0" },
+//             { Final_Grade: "O", Grade: "O", credits: "0" },
+//           ],
+//         })
+//         .end((err, res) => {
+//           if (err) done(err);
+//           expect(res).to.have.status(400);
+//           expect(res.body.message).to.equal("Unable to calculate cgpa now");
+//           done();
+//         });
+//     });
+//     it("{O,0},{O,0},{A+,0},{` `,0},{` `,0} => CGPA = Invalid", (done) => {
+//       chai
+//         .request(BASE_URL)
+//         .put("/student/caluclate/cgpa/anand123")
+//         .send({
+//           marks: [
+//             { Final_Grade: "O", Grade: "O", credits: "4" },
+//             { Final_Grade: "B+", Grade: "B+", credits: "4" },
+//             { Final_Grade: "A+", Grade: "A+", credits: "2" },
+//             { Final_Grade: " ", Grade: " ", credits: "1" },
+//             { Final_Grade: " ", Grade: " ", credits: "3" },
+//           ],
+//         })
+//         .end((err, res) => {
+//           if (err) done(err);
+//           expect(res).to.have.status(400);
+//           expect(res.body.message).to.equal("Unable to calculate cgpa now");
+//           done();
+//         });
+//     });
+//   });
+//   describe("positive condition", () => {
+//     it("{B+,4},{B,4},{A+,1},{A,3},{O,2} => CGPA = 8.32", (done) => {
+//       chai
+//         .request(BASE_URL)
+//         .put("/student/caluclate/cgpa/anand123")
+//         .send({
+//           marks: [
+//             { Final_Grade: "B+", Grade: "B", credits: "4" },
+//             { Final_Grade: "B", Grade: "B", credits: "4" },
+//             { Final_Grade: "A+", Grade: "A+", credits: "1" },
+//             { Final_Grade: "A", Grade: "A", credits: "3" },
+//             { Final_Grade: "O", Grade: "O", credits: "2" },
+//           ],
+//         })
+//         .end((err, res) => {
+//           if (err) done(err);
+//           expect(res).to.have.status(200);
+//           expect(res).to.be.an("object");
+//           expect(res.body.final_cgpa).to.equal("8.32");
+//           done();
+//         });
+//     });
+//   });
+// });
