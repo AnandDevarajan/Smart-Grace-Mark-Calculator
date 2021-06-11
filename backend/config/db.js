7ee43c9ed99c9a27ab2281bfdabc23a30411d698
Anand Devarajan <45593286+AnandDevarajan@users.noreply.github.com>
2021-06-11 14:07:34 +0530

    update db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,26 +1,14 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-if (process.env.STATUS === "online") {
-  config = {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: 34176,
-    multipleStatements: true,
-  };
-}
-
-if (process.env.STATUS === "localhost") {
-  config = {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    multipleStatements: true,
-  };
-}
+config = {
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: 34176,
+  multipleStatements: true,
+};
 
 const con = mysql.createConnection(config);
 con.connect((err) => {
